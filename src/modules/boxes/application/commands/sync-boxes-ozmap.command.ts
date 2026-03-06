7327d461fa54865f7bed188917689ee1f9e05e62
Matheus Felicio <matheus.felicio@grupoimagetech.com.br>
2026-03-06 18:15:57 -0400

    - Removidos manipuladores e casos de uso não utilizados no módulo de caixas - Simplificada a classe SyncBoxesOzmapCommand, removendo opções desnecessárias

--- src/modules/boxes/application/commands/sync-boxes-ozmap.command.ts
+++ src/modules/boxes/application/commands/sync-boxes-ozmap.command.ts
@@ -1,9 +1,5 @@
 import { Box } from "../../domain/entities/box.entity";
-import { SyncOptions } from "../types/sync-boxes-ozmap.types";
 
 export class SyncBoxesOzmapCommand {
-  constructor(
-    public readonly payload: Box[],
-    public readonly options?: SyncOptions,
-  ) {}
+  constructor(public readonly payload: Box[]) {}
 }
--- src/modules/boxes/boxes.module.ts
+++ src/modules/boxes/boxes.module.ts
@@ -3,9 +3,7 @@ import { CqrsModule } from "@nestjs/cqrs";
 import { CreateBoxHandler } from "./application/commands/create-box.handler";
 import { SyncBoxesOzmapHandler } from "./application/commands/sync-boxes-ozmap.handler";
 import { CreateBoxUseCase } from "./application/use-cases/create-box.use-case";
-import { CreateBoxesOzmapUseCase } from "./application/use-cases/create-boxes-ozmap.use-case";
 import { SyncBoxesOzmapUseCase } from "./application/use-cases/sync-boxes-ozmap.use-case";
-import { UpdateBoxesOzmapUseCase } from "./application/use-cases/update-boxes-ozmap.use-case";
 import { BOX_REPOSITORY } from "./domain/repositories/box.repository";
 import { BoxTypeormRepository } from "./infrastructure/repositories/box-typeorm.repository";
 import { OzmSdkModule } from "../ozm-sdk/ozm-sdk.module";
@@ -17,8 +15,6 @@ import { FailuresModule } from "../../common/failures/failures.module";
     CreateBoxHandler,
     SyncBoxesOzmapHandler,
     CreateBoxUseCase,
-    CreateBoxesOzmapUseCase,
-    UpdateBoxesOzmapUseCase,
     SyncBoxesOzmapUseCase,
     BoxTypeormRepository,
     {
