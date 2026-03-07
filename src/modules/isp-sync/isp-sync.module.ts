abfe118f4dd4bc111678f9bf651ae7a9f1e08827
Matheus Felicio <matheus.felicio@grupoimagetech.com.br>
2026-03-06 20:23:54 -0400

    - Adicionado módulos CablesModule, CustomersModule e DropCablesModule ao IspSyncModule - Removido IspSyncController do IspSyncModule - Removido OzmSdkController do ozm-sdk.module.ts

--- src/modules/isp-sync/isp-sync.module.ts
+++ src/modules/isp-sync/isp-sync.module.ts
@@ -2,24 +2,31 @@ import { HttpModule } from "@nestjs/axios";
 import { Module } from "@nestjs/common";
 import { CqrsModule } from "@nestjs/cqrs";
 import { BoxesModule } from "../boxes/boxes.module";
+import { CablesModule } from "../cables/cables.module";
+import { CustomersModule } from "../customers/customers.module";
+import { DropCablesModule } from "../drop-cables/drop-cables.module";
 import { RunIspSyncHandler } from "./application/commands/run-isp-sync.handler";
 import { RunOzmapSyncHandler } from "./application/commands/run-ozmap-sync.handler";
 import { RunIspSyncUseCase } from "./application/use-cases/run-isp-sync.use-case";
 import { RunOzmapSyncUseCase } from "./application/use-cases/run-ozmap-sync.use-case";
-import { IspSyncController } from "./presentation/controllers/isp-sync.controller";
 import { IspSyncCron } from "./presentation/crons/isp-sync.cron";
 
 @Module({
-  imports: [CqrsModule, HttpModule, BoxesModule],
-  controllers: [IspSyncController],
+  imports: [
+    CqrsModule,
+    HttpModule,
+    BoxesModule,
+    CablesModule,
+    CustomersModule,
+    DropCablesModule,
+  ],
+  controllers: [],
   providers: [
     RunIspSyncHandler,
     RunOzmapSyncHandler,
     RunIspSyncUseCase,
     RunOzmapSyncUseCase,
     IspSyncCron,
-    // { provide: ISP_SYNC_REPOSITORY, useClass: MongodbIspSyncRepository },
-    // { provide: OZMAP_GATEWAY, useClass: OzmapSdkGateway },
   ],
 })
 export class IspSyncModule {}
--- src/modules/ozm-sdk/ozm-sdk.module.ts
+++ src/modules/ozm-sdk/ozm-sdk.module.ts
@@ -1,12 +1,10 @@
 import { CqrsModule } from "@nestjs/cqrs";
 import { Module } from "@nestjs/common";
-import { OzmSdkController } from "./presentation/controllers/ozm-sdk.controller";
 import { OzmSdkAuthService } from "./infrastructure/services/ozm-sdk-auth.service";
 import { OZM_SDK_AUTH_SERVICE } from "./domain/interfaces/ozm-sdk.service.interface";
 
 @Module({
   imports: [CqrsModule],
-  controllers: [OzmSdkController],
   providers: [
     OzmSdkAuthService,
     {
